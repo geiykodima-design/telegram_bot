adc60a3ab93f502511211ee23108acd6a24b35d5
Dmytro <geiykodima@gmail.com>
2026-02-02 19:27:17 +0000

    Add bot.py main script

---
+++ src/bot.py
@@ -0,0 +1,14 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler
+
+from config import TELEGRAM_TOKEN
+from handlers import start
+
+app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling(
+    drop_pending_updates=True,
+    allowed_updates=Update.ALL_TYPES
+)
